38c445f266841b6b3375b903a045036845fe1e9b
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2024-04-18 15:21:44 +0500

    q11 commit

---
+++ Q_11/main.js
@@ -0,0 +1,8 @@
+/*
+Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+*/
+let names = ["Ghulam Murtaza", "Shahid Ali", "Sohail", "Zulfiqar Ali"];
+for (let val of names) {
+    console.log(val);
+}
+export {};
