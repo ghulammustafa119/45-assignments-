18044621f6863546dc7da713824383d72377ec1b
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2024-04-18 13:17:56 +0500

    q4 commit

---
+++ Q_4/main.ts
@@ -0,0 +1,4 @@
+/*
+Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+*/
+console.log(`Malcolm x once said,"Education is the passport to the future, for tomorrow belongs to those who prepare for it today."`);
