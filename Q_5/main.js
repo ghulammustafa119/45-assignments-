63363715049d7ffa4a219ee740232a0d070e3b6f
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2024-04-18 13:39:10 +0500

    q5 commit

---
+++ Q_5/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+*/
+let famous_person = "Malcolm X";
+let message = `"Education is the passport to the future, for tomorrow belongs to those who prepare for it today."`;
+console.log(`${famous_person} once said, ${message}`);
