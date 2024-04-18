8453eb1196b496a4763f03849cdfb6b9da9af7a8
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2024-04-18 12:51:22 +0500

    q3 commit

---
+++ Q_3/index.ts
@@ -0,0 +1,11 @@
+/*
+Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+*/
+let personName : string = "ghulam mustaFA BHUtto";
+console.log(`Name in uppercase : ${personName.toUpperCase()}`);
+console.log(`Name in lowercase : ${personName.toLowerCase()}`);
+// for titlecase
+let step1 = personName.split(" ");
+let step2 = step1.map(word => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase());
+let step3 = step2.join(" ");
+console.log(`Name in titlecase : ${step3}`);
