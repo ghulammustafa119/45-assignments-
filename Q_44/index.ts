491ebb3447739e98ff4898a8aa3d2b655c277f7e
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2024-04-22 09:03:36 +0500

    q44 commit

---
+++ Q_44/index.ts
@@ -0,0 +1,10 @@
+/*
+Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+*/
+function makeSandWich (item:string[]){
+ console.log("\nmaking your sandwich wuth:");
+ item.forEach((element) => console.log("- " + element)); 
+ }
+makeSandWich(["Ham", "Cheese", "Lttuce"]);
+makeSandWich(["Turkey", "Bacon"]);
+makeSandWich(["Peanut", "jelly"]);
