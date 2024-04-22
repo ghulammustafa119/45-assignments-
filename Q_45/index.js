d3b6e23bb950db1e5802d34d2b2ce8468136c809
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2024-04-22 10:28:40 +0500

    q45 commit

---
+++ Q_45/index.js
@@ -0,0 +1,14 @@
+/*
+Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly
+*/
+function carInfo(manufacturer, modelName, ...extraOption) {
+    const carInfo = {
+        manufacturer,
+        modelName,
+        ...Object.assign({}, ...extraOption)
+    };
+    return carInfo;
+}
+let result = carInfo('Honda', 'civic', { color: 'black' }, { features: ['Navigation', 'Power window'] });
+console.log(result);
+export {};
