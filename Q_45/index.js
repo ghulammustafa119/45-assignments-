0393e241270cbb563587a049f9012ac574595da2
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2024-05-24 15:47:32 +0500

    commit

--- Q_45/index.js
+++ Q_45/index.js
@@ -1,14 +1,17 @@
 /*
 Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly
 */
-function carInfo(manufacturer, modelName, ...extraOption) {
-    const carInfo = {
-        manufacturer,
-        modelName,
-        ...Object.assign({}, ...extraOption)
+function carInfo(manufacturer, model, ...options) {
+    let car = {
+        manufacturer: manufacturer,
+        model: model
     };
-    return carInfo;
+    options.forEach(option => {
+        let [key, value] = option.split(":");
+        car[key.trim()] = value.trim();
+    });
+    return car;
 }
-let result = carInfo('Honda', 'civic', { color: 'black' }, { features: ['Navigation', 'Power window'] });
-console.log(result);
+let myCar = carInfo("Toyota", "Corolla", "color:red", "year:2024", "type:sedan");
+console.log(myCar);
 export {};
--- Q_45/index.ts
+++ Q_45/index.ts
@@ -1,16 +1,17 @@
-
-
 import { features } from "process";
 /*
 Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly
 */
-function carInfo(manufacturer: string,modelName: string, ...extraOption:{[key:string]:any}[]):object{
-    const carInfo = {
-        manufacturer,
-        modelName,
-        ...Object.assign({}, ...extraOption)
-       }
-       return carInfo;
+function carInfo(manufacturer: string, model: string, ...options: string[]): { [key: string]: string } {
+    let car: { [key: string]: string } = {
+        manufacturer: manufacturer,
+        model: model
+    };
+    options.forEach(option => {
+        let [key, value] = option.split(":");
+        car[key.trim()] = value.trim();
+    });
+    return car;
 }
-let result = carInfo('Honda', 'civic',{color:'black'},{features:['Navigation', 'Power window']});
-console.log(result);
+let myCar = carInfo("Toyota", "Corolla", "color:red", "year:2024", "type:sedan");
+console.log(myCar);
